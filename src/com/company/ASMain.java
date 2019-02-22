b979ac2f0e5802e3309ed917ed9f106da79a2be1
Eric Strong <c15708709@mydit.ie>
2019-02-22 10:51:51 +0000

    sec lab2

---
+++ src/com/company/ASMain.java
@@ -0,0 +1,13 @@
+/*********************************************************/
+/*** Date : Dec, 12th, 2011                            ***/
+/*** Description: Main class to start the program      ***/
+/*********************************************************/
+package com.company;
+
+public class ASMain
+{
+	public static void main(String args[])
+	{
+		new ASFrame("Meng Cui");
+	}
+}
